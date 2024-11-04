80801dbbb04c6ed5515d10697746d90c2ef4d485
monqieu <moniquesiu319@gmail.com>
2024-11-04 20:27:17 +0800

    Removed redundant comments.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,24 @@ public class Main {
      * int[] myArray2 = {2,4,6,8,10,12,14,16};
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
-    public static int[] combine(int[] array1, int[] array2) {
 
-        return null;
+    //since we are writing a METHOD that combines two given arrays, the two arrays to be combined should be the two parameters of the method
+    public static int[] combine(int[] array1, int[] array2){
+        int[] combinedArray =  new int[array1.length + array2.length];  //we need to know the total length of the new array when combining it
 
+        //adding the elements from array1 to combinedArray first, according to instructions
+        for(int i = 0; i < array1.length; i++){
+            combinedArray[i] = array1[i];
+        }
+
+        //adding elements from array2 to combinedArray after the values of array1
+        for(int i = 0; i < array2.length; i++){
+            combinedArray[array1.length + i] = array2[i];
+        }
+        return combinedArray;
     }
+
+
 
     /**
      * Given two arrays of integers of equal length, write a method called zip that zips together all elements in each array into one larger array.
@@ -20,11 +33,27 @@ public class Main {
      * int[] myArray2 = {2,4,6,8,10};
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
-    public static int[] zip(int[] array1, int[] array2) {
 
-        return null;
+    public static int[] zip(int[] array1, int[] array2){
+        //instantiating zippedArray, with its number elements combining that of array1 and array2
+        int[] zippedArray = new int[array1.length + array2.length];
+        //creating variable length for limiting range of loops
+        int length = array1.length + array2.length;
 
+        for(int i = 0; i<length; i++){
+            zippedArray[i*2 + 1] = array1[i];
+            // since this for loop assigns elements from array1 to zippedArray, elements are odd numbers
+            //similar to arithmetic sequences, index is calculated as i*2+1, places elements from array1 at odd indices (1,3,5...)
+        }
+
+        for(int i = 0; i<length; i++){
+            zippedArray[i*2] = array2[i];
+            //array 2 is even numbers, so index calculated as i*2 places elements at even indices (2, 4, 6..)
+        }
+        return zippedArray;
     }
+
+
 
     /**
      * Given two arrays of integers of equal length, write a method called product that multiplies each element in the first array by the element at the corresponding index in the second array.
@@ -34,9 +63,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] product = new int[array1.length];
+        for(int i = 0; i < array1.length; i++){
+            product[i] = array1[i] * array2[i];
+        }
+        return product;
     }
 
     /**
@@ -55,15 +86,36 @@ public class Main {
      * String[] words = {“Christmas”, “IS”, “comInG”, “!”};
      * capitalCount(words) → {1, 2, 2, 0}
      */
-    public static int[] capitalCount(String[] words) {
 
-        return null;
+    public static int[] capitalCount(String[] words) {
+        //create integer array with same length as words array
+        int[] result = new int[words.length];
+
+        //loop through each word in the words array
+        for(int i = 0; i < words.length; i++){
+            result[i] = countCapitalLetters(words[i]);
+            //for each word in the words array, call helper method and store the resulting value in the corresponding position in the result array
+        }
+        return result;
 
     }
 
     public static int countCapitalLetters(String word) {
+        //count variable is initialized to count capital letters later on
+        int count = 0;
 
-        return 0;
+        //loop through each CHARACTER in each word
+        for( int i = 0; i < word.length(); i++){
+            //obtaining the current character and storing it in char variable c
+            char c = word.charAt(i);
+            //condition: if the current character is alphabetic AND it is upper case, then count c will increase by 1
+            if(Character.isAlphabetic(c) && Character.isUpperCase(c)){
+                count ++;
+            }
+        }
+
+        //returning total count of capital letters found
+        return count;
 
     }
 
